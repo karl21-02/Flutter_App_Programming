32c356ff9de58ac44f621f51773b1758d4bff773
kimjunhee <manuna530@gmail.com>
2024-12-29 10:32:41 +0900

    Network http

---
+++ largefile_download/android/app/src/main/kotlin/com/example/largefile_download/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.largefile_download
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ todo_list/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/thirdPage.dart';
+import 'package:todo_list/subDetail.dart';
+import 'package:todo_list/secondDetail.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  static const String _title = 'Widget Example';
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: _title,
+      initialRoute: '/',
+      routes: { // 페이지를 등록한다는 개념
+        '/': (context) => SubDetail(),
+        '/second': (context) => SecondDetail(),
+        '/third': (context) => ThirdDetail()
+      },
+    );
+  }
+}
